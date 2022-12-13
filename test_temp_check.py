53456086790919d267401c6e638889ab4e627c81
Singh Om Prakash (MS/ESX6-XC) <iom2kor@bosch.com>
2022-12-13 15:08:30 +0530

    changing test_temp_check script

--- test_temp_check.py
+++ test_temp_check.py
@@ -1,12 +1,12 @@
 from Test_temp_type import temp_type
 
 def temp_check(threshold_dict_data, test_case):
+    # Convert temperature type to Celcius or Fahrenheit depending upon the battery threshold temperature type
     check_present = True
     temperature = test_case["Temperature"]
     if threshold_dict_data["Temp_Type"] != test_case["Temp_Type"]:
         temperature = temp_type(test_case["Temp_Type"], test_case["Temperature"])
-    # Convert temperature type to Celcius or Fahrenheit depending upon the battery threshold temperature type
-    if temperature < threshold_dict_data["Charge_Temp_Min"] or temperature > threshold_dict_data["Charge_Temp_Max"]:
+    elif temperature < threshold_dict_data["Charge_Temp_Min"] or temperature > threshold_dict_data["Charge_Temp_Max"]:
         check_present = False
     return check_present
 
